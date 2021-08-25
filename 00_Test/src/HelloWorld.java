650bbab2db37695d39b06f71b566ca2f5588179f
BDH <baekdh@iei.or.kr>
2021-08-25 18:01:54 +0900

    2021.08.25 수업내용

---
+++ 00_Test/src/HelloWorld.java
@@ -0,0 +1,17 @@
+// 한 줄 주석
+
+/* 범위 주석
+주석이 
+없으면 
+에러가 
+납니다
+*/
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		System.out.println("오늘 수업 5분 남았습니다");
+		System.out.println("오늘 수업 5분 남았습니다");
+		System.out.println("오늘 수업 5분 남았습니다");
+	}
+}
